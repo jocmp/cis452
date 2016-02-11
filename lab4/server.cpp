c476dc4a13ebfcd6f9bc94a7a03a5ab38d9c1d82
Josiah Campbell <campbjos@mail.gvsu.edu>
2016-02-10 23:06:16 -0500

    Update with sighandler

--- lab4/server.cpp
+++ lab4/server.cpp
@@ -2,51 +2,53 @@
 #include <stdlib.h>
 #include <iostream>
 #include <thread>
-#include <future>
+#include <signal.h>
 
 #define RUNNING 1
 
 using namespace std;
 // Prototypes
-void find_file_low(char* arg);
-void *find_file_high(void *arg);
+void *find_file(string arg);
+void sig_handler(int sig_num);
 
 int serviced;
-int prob[5] = { 1, 1, 1, 0, 1};
+bool accepting_input;
 
 int main(int argc, char* argv[]) {
-    int status;
-    int prob_count = 1;
     serviced = 0;
+    accepting_input = true; 
+    signal(SIGINT, sig_handler);
     while (RUNNING) {
         string input;
-        char* c_input;
-        cout << "Enter a filename: ";
-        cin >> input;
-        c_input = const_cast<char*>(input.c_str());
-        if (prob[prob_count]) {
-              thread low(find_file_low, const_cast<char*>(input.c_str()));
-              low.detach();
-        } else {
-            thread high(find_file_high, c_input);
-            high.detach();
+        if (accepting_input) {
+            cout << "Enter a filename: " << flush;
+            cin >> input;
+            thread file(find_file, input); 
+            // Thread can run independently of dispatcher
+            // and free memory after finishing
+            file.detach(); 
         }
-
-        prob_count == 5 ? prob_count = 1 : prob_count++;
     }
     return 0;
 }
 
-void find_file_low(char* arg) {
-    const char *val_ptr = arg;
-    sleep((rand() % 4) + 7);
-    cout << endl << "Couldn't find " << val_ptr << ". Sorry." << endl;
+void sig_handler(int sig_num) {
+    cout << " received" << endl;
+    accepting_input = false;
+    cout << "We handled " << serviced << " file queries." << endl;
+    exit(0);
 }
 
-/* High probability routine */
-void *find_file_high(void *arg) {
-    const char *val_ptr = (char *)arg;
-    sleep(1);
-    cout << endl << "Found " << val_ptr << ".docx" << endl;
+void *find_file(string arg) {
+    int probability = rand() % 101;
+    if (probability > 20) {
+        sleep(1);
+        cout << endl << "Found " << arg << ".docx" << endl;
+    } else {
+        sleep((rand() % 4) + 7);
+        cout << endl << "Couldn't find " << arg << ".jsf Sorry." << endl;
+    }
+    // We handled another file. 
+    serviced++;
     return NULL;
 }
